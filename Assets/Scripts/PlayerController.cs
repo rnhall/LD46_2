fcb1e784393e332edaf4f2b0674c443424ce7400
rnhall <nhalldna@gmail.com>
2020-04-18 23:31:26 -0700

    Added club animator

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("swing", true);
+        } else
+        {
+            animator.SetBool("swing", false);
+        }
+        
+    }
+}
