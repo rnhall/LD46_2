dd9f1c1aae2fda5554f7ed060bd9f66868e461a8
rnhall <nhalldna@gmail.com>
2020-04-19 09:16:31 -0700

    Added sounds effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,16 @@ public class PlayerController : MonoBehaviour
     public Animation animation;
     public Camera camera;
 
+    private AudioSource audioSource;
+    public AudioClip[] hitSounds;
+    private AudioClip hitClip; 
+
     public float attackRange;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -38,6 +42,11 @@ public class PlayerController : MonoBehaviour
             {
                 GameObject hitObj = hit.transform.gameObject;
                 hitObj.GetComponent<CustomerAI>().MakeRagdoll();
+                hitObj.GetComponent<Rigidbody>().AddForce(this.transform.forward*10, ForceMode.Force);
+                int index = Random.Range(0, hitSounds.Length);
+                hitClip = hitSounds[index];
+                audioSource.clip = hitClip;
+                audioSource.Play();
             }
         }
 
