73671a56cefc8d34f8cd0f0142e3abe76c9d8be8
Matt <mjmccoy@stanford.edu>
2020-04-19 14:14:59 -0700

    Post-apocolyptic commit
    Post-apocolyptic commit

---
+++ Assets/Scripts/GrowBigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowBigger : MonoBehaviour
+{
+    Vector3 size;
+    public float timeT = 0;
+    bool recentlyFed = false;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Food")
+        {
+            recentlyFed = true;
+            Destroy(collider.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (recentlyFed == true)
+        {
+            timeT += Time.deltaTime;
+            size = transform.localScale;
+            size.x += 0.05f;
+            size.y += 0.05f;
+            size.z += 0.05f;
+            transform.localScale = size;
+            if (timeT > 1)
+            {
+                recentlyFed = false;
+                timeT = 0;
+            }
+        }
+    }
+}
