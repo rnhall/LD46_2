380b5920e19d1045650cb16350abdd72ed1d5532
rnhall <nhalldna@gmail.com>
2020-04-19 21:10:33 -0700

    Fixed menu and monster dragging behavior

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -1,25 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MonsterController : MonoBehaviour
 {
     public Animator monsterAnim;
     public bool FeedMe = false;
     public bool Omnomnom = false;
-    public GameObject player;
+    public PlayerController player;
+    public int numFed;
     //public float feedtimeT = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        numFed = 0;
         monsterAnim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(numFed == 3)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 
     void OnTriggerEnter(Collider collider)
@@ -29,6 +35,7 @@ public class MonsterController : MonoBehaviour
             //Omnomnom = true;
             player.GetComponent<PlayerController>().isDragging = false;
             monsterAnim.Play("Omnomnom");
+            numFed += 1;
             //monsterAnim.Play("Omnomnom");    
         }
     }
