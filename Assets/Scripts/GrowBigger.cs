bbbf4f9e33bef390f25fb509aba4a6954127d76a
rnhall <nhalldna@gmail.com>
2020-04-19 20:24:22 -0700

    misc

--- Assets/Scripts/GrowBigger.cs
+++ Assets/Scripts/GrowBigger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GrowBigger : MonoBehaviour
 {
     Vector3 size;
+    Vector3 newposition;
     public float timeT = 0;
     bool recentlyFed = false;
 
@@ -23,10 +24,13 @@ public class GrowBigger : MonoBehaviour
         {
             timeT += Time.deltaTime;
             size = transform.localScale;
-            size.x += 0.05f;
-            size.y += 0.05f;
-            size.z += 0.05f;
+            size.x += 0.01f;
+            size.y += 0.01f;
+            size.z += 0.01f;
             transform.localScale = size;
+            newposition = transform.position;
+            newposition.y += 0.015f;
+            transform.position = newposition;
             if (timeT > 1)
             {
                 recentlyFed = false;
