e07f8e43569b30c5e23e767e88c9f49170c2f0f9
Matt <mjmccoy@stanford.edu>
2020-04-19 21:32:38 -0700

    FeedMeee audio
    FeedMeee audio

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -5,31 +5,39 @@ using UnityEngine;
 public class MonsterController : MonoBehaviour
 {
     public Animator monsterAnim;
+    private AudioSource monsteraudioSource;
+    private AudioClip monsterhitClip;
+    //public AudioClip monsterAudio;
     public bool FeedMe = false;
     public bool Omnomnom = false;
     public GameObject player;
-    //public float feedtimeT = 0;
+    public float feedtimeT = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         monsterAnim = gameObject.GetComponent<Animator>();
+        monsteraudioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        feedtimeT += Time.deltaTime;
+        if (feedtimeT > 10)
+        {
+            monsterAnim.Play("FeedMe");
+            monsteraudioSource.Play();
+            feedtimeT = 0;
+        }
     }
 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Food")
         {
-            //Omnomnom = true;
             player.GetComponent<PlayerController>().isDragging = false;
-            monsterAnim.Play("Omnomnom");
-            //monsterAnim.Play("Omnomnom");    
+            monsterAnim.Play("Omnomnom");  
         }
     }
 
